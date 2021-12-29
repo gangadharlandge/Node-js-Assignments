c933045a772fd1c63b020ea2d8bbb11c61901bfa
gangadhar <gangadharlandge8@gmail.com>
2021-12-28 17:29:35 -0800

    adding files

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,13 +1,24 @@
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
+    const process=require('process')
+    return process.argv[2]
 }
 
 function getNameFromEnv() {
     // Write your code here
+    const process=require('process')
+    return process.env.name
 }
 
 function getNameFromReadLine() {
     // Write your code here
+    const readline=require('readline');
+    var rl = readline.createInterface(
+        process.stdin, process.stdout);
+        rl.question((name) => {
+            return name
+        });
+    rl.close();
 }
 
 module.exports = {
